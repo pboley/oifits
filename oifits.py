c6bdd4820669148be62675e0628fabe3c80e59fd
Paul Boley <pboley@gmail.com>
2026-02-17 10:57:46 +0100

    Read and write checksums and datasums by default

--- oifits.py
+++ oifits.py
@@ -1383,7 +1383,7 @@ class oifits(object):
                     inspol.info()
             print("%d inspol measurement%s"%(len(self.inspol), _plurals(len(self.inspol))))
 
-    def save(self, filename, overwrite=False):
+    def save(self, filename, overwrite=False, checksum=True):
         """Write the contents of the oifits object to a file in OIFITS
         format."""
 
@@ -1907,11 +1907,11 @@ class oifits(object):
                 else: hdr['CALSTAT'] = 'U', 'Calibration status'
                 hdulist.append(hdu)
 
-        hdulist.writeto(filename, overwrite=overwrite)
+        hdulist.writeto(filename, overwrite=overwrite, checksum=checksum)
 
 
 
-def open(filename, quiet=False):
+def open(filename, quiet=False, checksum=True):
     """Open an OIFITS file."""
 
     newobj = oifits()
@@ -1923,7 +1923,7 @@ def open(filename, quiet=False):
     if type(filename) == fits.hdu.hdulist.HDUList:
         hdulist = filename
     else:
-        hdulist = fits.open(filename)
+        hdulist = fits.open(filename, checksum=checksum)
     # Save the primary header
     newobj.header = hdulist[0].header.copy()
 
